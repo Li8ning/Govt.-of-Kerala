4da91d4b169753e3d379640c4d1d44a4a1fdf78a
Li8ning <jadejadharmraj5@gmail.com>
2021-05-18 19:22:30 +0530

    18-5-21 v2

--- assests/js/style.js
+++ assests/js/style.js
@@ -1,8 +1,10 @@
-var chk = document.getElementById('chk');
-
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+$('#chk').change(function(){
+	$('body').toggleClass('dark');
+	$('.label').toggleClass('dark');
+	$('.lang-btn').toggleClass('dark');
+	$('.font-size-btn li a').toggleClass('dark');
+	$('.main-title h1').toggleClass('dark');
+	$('.main-title h4').toggleClass('dark');
+	$('.light-img').toggle();
+	$('.dark-img').toggle();
 });
-
-// var about_dropdown = document.getElementById('about-dropdown');
-// $('#about-dropdown').hover();
