1947aea03293adbc543f30748382467365eaf87c
Li8ning <jadejadharmraj5@gmail.com>
2021-05-19 16:04:21 +0530

    audit-ready 19-5-21

--- assests/js/style.js
+++ assests/js/style.js
@@ -7,7 +7,7 @@ function dark_mode(class_name){
 
 $(document).ready(function(){
 	$('#chk').change(function(){
-		var class_name = ["body",".label",".lang-btn",".font-size-btn li a",".main-title h1",".main-title h4",".light-img",".dark-img",".search",".search_input",".main-nav-menu .dropdown-menu",".main-nav-menu a.nav-link",".main-nav-menu .dropdown-menu a.dropdown-item",".main-nav-menu .dropdown-menu .dropdown-item:hover",".kg-alert",".img-outer",".section-what-new #col-gov-cm",".section-articles .article-author",".head-dark",".section-what-new #col-new-updates",".section-what-new #col-new-updates .table-cell",".footer-top",".footer-text",".footer-bottom"];
+		var class_name = ["body","header",".label",".lang-btn",".font-size-btn li a",".main-title h1",".main-title h4",".light-img",".dark-img",".search",".search_input",".main-nav-menu .dropdown-menu",".main-nav-menu a.nav-link",".main-nav-menu .dropdown-menu a.dropdown-item",".main-nav-menu .dropdown-menu .dropdown-item:hover",".kg-alert",".img-outer",".img-border",".section-what-new #col-gov-cm",".section-articles .article-author",".head-dark",".section-what-new #col-new-updates",".section-what-new #col-new-updates .table-cell",".footer-top",".footer-text",".footer-bottom"];
 		dark_mode(class_name);
 	});
 	$('[data-toggle="tooltip"]').tooltip();
