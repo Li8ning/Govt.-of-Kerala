98cb434097701be0589c82e057889499e27d6f07
Li8ning <jadejadharmraj5@gmail.com>
2021-05-19 12:57:16 +0530

    19-5-21 dark mode

--- assests/js/style.js
+++ assests/js/style.js
@@ -1,19 +1,21 @@
+function dark_mode(class_name){
+	class_name.forEach(element => {
+		$(element).toggleClass('dark');
+	});
+}
+
+
 $(document).ready(function(){
 	$('#chk').change(function(){
-		$('body').toggleClass('dark');
-		$('.label').toggleClass('dark');
-		$('.lang-btn').toggleClass('dark');
-		$('.font-size-btn li a').toggleClass('dark');
-		$('.main-title h1').toggleClass('dark');
-		$('.main-title h4').toggleClass('dark');
-		$('.light-img').toggle();
-		$('.dark-img').toggle();
+		var class_name = ["body",".label",".lang-btn",".font-size-btn li a",".main-title h1",".main-title h4",".light-img",".dark-img",".search",".search_input",".main-nav-menu .dropdown-menu",".main-nav-menu a.nav-link",".main-nav-menu .dropdown-menu a.dropdown-item",".main-nav-menu .dropdown-menu .dropdown-item:hover",".kg-alert",".img-outer",".section-what-new #col-gov-cm",".section-articles .article-author",".head-dark",".section-what-new #col-new-updates",".section-what-new #col-new-updates .table-cell",".footer-top",".footer-text",".footer-bottom"];
+		dark_mode(class_name);
 	});
 	$('[data-toggle="tooltip"]').tooltip();
 	$(window).scroll(function () {
 		if ($(this).scrollTop() + $(window).height() > $(document).height() - 100)  {
 			$('#btn-to-top,#btn-cm-live').fadeIn();
-		}  else {
+		}
+		else {
 			$('#btn-to-top, #btn-cm-live').fadeOut();
 		}
 	});
