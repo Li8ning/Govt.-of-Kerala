8db49cc0e9ddb6868d867d62c81f1602fee070ca
Li8ning <jadejadharmraj5@gmail.com>
2021-05-21 11:42:49 +0530

    initial-desk-changes

--- assests/js/style.js
+++ assests/js/style.js
@@ -6,7 +6,7 @@ function dark_mode(class_name){
 
 
 $(document).ready(function(){
-	var $font_ele_collection = $("body,.kg-p-font,.kg-s-font,.kg-t-font,.kg-f-font");
+	var $font_ele_collection = $("body,.kg-p-font,.kg-s-font,.kg-t-font,.kg-f-font,.kg-ff-font,.kg-h-font,h1,h2,h3,h4,h5");
 	var $font_counter = 0;
 	$font_ele_collection.each( function(){
 		var $this = $(this);
@@ -46,16 +46,17 @@ $(document).ready(function(){
 	}
 
 	$('#chk').change(function(){
-		var class_name = ["body","header",".label",".lang-btn",".font-size-btn li a",".main-title h1",".main-title h4",".light-img",".dark-img",".search",".search_input",".main-nav-menu .dropdown-menu",".main-nav-menu a.nav-link",".main-nav-menu .dropdown-menu a.dropdown-item",".main-nav-menu .dropdown-menu .dropdown-item:hover",".kg-alert",".img-outer",".img-border",".section-what-new #col-gov-cm",".section-articles .article-author",".head-dark",".section-what-new #col-new-updates",".section-what-new #col-new-updates .table-cell",".footer-top",".footer-text",".footer-bottom"];
+		var class_name = ["body","header",".label",".lang-btn",".font-size-btn li a",".main-title h1",".main-title h4",".light-img",".dark-img",".search",".search_input",".main-nav-menu .dropdown-menu",".main-nav-menu a.nav-link",".main-nav-menu .dropdown-menu a.dropdown-item",".main-nav-menu .dropdown-menu .dropdown-item:hover",".kg-alert",".img-outer",".img-border",".section-what-new #col-gov-cm",".section-articles .article-author",".head-dark",".section-what-new #col-new-updates",".section-what-new #col-new-updates .table-cell",".footer-top",".footer-text",".footer-bottom,#youtube-toast .toast-body"];
 		dark_mode(class_name);
 	});
-	$('[data-toggle="tooltip"]').tooltip();
 	$(window).scroll(function () {
 		if ($(this).scrollTop() + $(window).height() > $(document).height() - 100)  {
 			$('#btn-to-top,#btn-cm-live').fadeIn();
+			$('#youtube-toast').toast('show');
 		}
 		else {
-			$('#btn-to-top, #btn-cm-live').fadeOut();
+			$('#btn-to-top,#btn-cm-live').fadeOut();
+			$('#youtube-toast').toast('hide');
 		}
 	});
 	// scroll body to 0px on click
