1b25dc7db0ee056d43c8517e54913cd1efec7885
Li8ning <jadejadharmraj5@gmail.com>
2021-05-22 14:22:04 +0530

    mobile-changes 22-5-21

--- assests/js/style.js
+++ assests/js/style.js
@@ -1,11 +1,47 @@
+// Dark Mode Function
 function dark_mode(class_name){
 	class_name.forEach(element => {
 		$(element).toggleClass('dark');
 	});
 }
 
+$(document).change(function(){
+	if ($(window).width()<992) {
+		console.log($(".dropdown-menu").hasClass("show"));
+		if ($(".dropdown-menu").hasClass("show")) {
+			// $(this).find(".dropdown-menu").toggleClass("show");
+			$(this).find(".dropdown-toggle").toggleClass("active");
+			console.log($(this).find(".dropdown-menu").length);
+		}
+	}
+});
 
 $(document).ready(function(){
+
+	// For Screen Size>1024
+	if ($(window).width()>=992) {
+		$(".dropdown").hover(function(){
+			$(this).find(".dropdown-menu").toggleClass("show");
+			$(this).find(".dropdown-toggle").toggleClass("active");
+			// console.log($(this).find(".dropdown-menu").length);
+		});
+	}
+	// else if ($(window).width()<992) {
+	// 	$(".dropdown").click(function(){
+	// 		if ($(this).find(".dropdown-menu").hasClass("show")) {
+	// 			// console.log($(this).find(".dropdown-toggle"));
+	// 			// $(this).find(".dropdown-toggle").focus(function(){
+	// 			// 	console.log("found");
+	// 			// });
+	// 		}
+	// 		else{
+	// 			$(this).find(".dropdown-menu").removeClass("show");
+	// 		}
+	// 	});
+	// }
+
+
+	// Change Font Functionality
 	var $font_ele_collection = $("body,.kg-p-font,.kg-s-font,.kg-t-font,.kg-f-font,.kg-ff-font,.kg-h-font,h1,h2,h3,h4,h5");
 	var $font_counter = 0;
 	$font_ele_collection.each( function(){
@@ -45,10 +81,14 @@ $(document).ready(function(){
 		});
 	}
 
+
+	// Dark Mode Activator
 	$('#chk').change(function(){
 		var class_name = ["body","header",".label",".lang-btn",".font-size-btn li a",".main-title h1",".main-title h4",".light-img",".dark-img",".search",".search_input",".main-nav-menu .dropdown-menu",".main-nav-menu a.nav-link",".main-nav-menu .dropdown-menu a.dropdown-item",".main-nav-menu .dropdown-menu .dropdown-item:hover",".kg-alert",".img-outer",".img-border",".section-what-new #col-gov-cm",".section-articles .article-author",".head-dark",".section-what-new #col-new-updates",".section-what-new #col-new-updates .table-cell",".footer-top",".footer-text",".footer-bottom,#youtube-toast .toast-body"];
 		dark_mode(class_name);
 	});
+
+	// Bottom right buttons hide/show on scroll
 	$(window).scroll(function () {
 		if ($(this).scrollTop() + $(window).height() > $(document).height() - 100)  {
 			$('#btn-to-top,#btn-cm-live').fadeIn();
